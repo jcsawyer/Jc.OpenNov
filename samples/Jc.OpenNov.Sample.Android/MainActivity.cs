c058c8890996d4eb422e90f9b13fd820bc7ef2b2
Joseph Sawyer <joseph@jcsawyer.com>
2025-05-24 17:38:55 +0100

    wip: add android NFC reading

--- samples/Jc.OpenNov.Sample.Android/MainActivity.cs
+++ samples/Jc.OpenNov.Sample.Android/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Content.PM;
 using Avalonia;
 using Avalonia.Android;
+using Jc.OpenNov.Nfc.Android;
 
 namespace Jc.OpenNov.Sample.Android;
 
@@ -13,9 +14,12 @@ namespace Jc.OpenNov.Sample.Android;
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public class MainActivity : AvaloniaMainActivity<App>
 {
+    internal static NfcController NfcController;
+    
     protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
     {
         return base.CustomizeAppBuilder(builder)
+            .UseOpenNovo(this)
             .WithInterFont();
     }
 }
