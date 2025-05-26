dfa98599d1a32ade553064ea979c95e01866c0a6
Joseph Sawyer <jcsawyer@Josephs-MacBook-Pro-2.local>
2025-05-26 09:51:32 +0100

    cleanup and use jcs unit test data

--- src/Jc.OpenNov.Tests/NvpControllerTests.cs
+++ src/Jc.OpenNov.Tests/NvpControllerTests.cs
@@ -11,7 +11,7 @@ internal sealed class NvpControllerTests
     [SetUp]
     public void SetUp()
     {
-        _testFilePath = "nvp_datatest.txt"; // Ensure file is copied to output dir
+        _testFilePath = "datatest.txt";
         var stream = File.OpenRead(_testFilePath);
         _controller = new NvpController(new TestingDataReader(stream));
     }
@@ -30,15 +30,15 @@ internal sealed class NvpControllerTests
         
         Assert.Multiple(() =>
         {
-            Assert.That(data.Serial, Is.EqualTo("ABERGX"));
-            Assert.That(data.Doses.Count, Is.EqualTo(96));
-            Assert.That(data.StartTime, Is.EqualTo(6206432L));
+            Assert.That(data.Serial, Is.EqualTo("ACUKKE"));
+            Assert.That(data.Doses.Count, Is.EqualTo(23));
+            Assert.That(data.StartTime, Is.EqualTo(462022L));
 
-            var expectedUnits = new[] { 20, 20, 100, 160, 140, 20 };
+            var expectedUnits = new[] { 60, 100, 50, 10, 160, 90 };
             var actualUnits = data.Doses.Take(6).Select(d => d.Units).ToArray();
             Assert.That(actualUnits, Is.EqualTo(expectedUnits));
 
-            var expectedTimes = new long[] { -86, -87, -90, -92, -260474, -260480 };
+            var expectedTimes = new long[] { -3591, -4188, -5597, -6098, -6120, -6225 };
             var actualTimes = data.Doses.Take(6)
                 .Select(d => (d.Time - currentTime) / 1000)
                 .ToArray();
@@ -60,7 +60,7 @@ internal sealed class NvpControllerTests
 
         Assert.Multiple(() =>
         {
-            Assert.That(data.Serial, Is.EqualTo("ABERGX"));
+            Assert.That(data.Serial, Is.EqualTo("ACUKKE"));
             Assert.That(data.Doses.Count, Is.EqualTo(18));
         });
     }
